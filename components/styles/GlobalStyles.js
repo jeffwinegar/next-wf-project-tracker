b37c5494684260909639351de6d671ec30d709c9
Jeff Winegar <winegarj@gmail.com>
2021-04-06 15:42:05 -0700

    wip: start of global styles

--- components/styles/GlobalStyles.js
+++ components/styles/GlobalStyles.js
@@ -1,3 +1,18 @@
 import { createGlobalStyle } from 'styled-components';
 
-export const GlobalStyle = createGlobalStyle``;
+export const GlobalStyle = createGlobalStyle`
+  :root {
+    --black: #424344;
+    --darkGray: #5a5e61;
+    --mediumGray: #a8abaf;
+    --gray: #d9dce1;
+    --lightGray: #f2f3f5;
+    --offWhite: #f7f7f9;
+    --white: #ffffff;
+    --wtBlue: #0a47ed;
+    --wfOrange: #f7931e;
+
+    --shadow: 0px 3px 8px -3px var(--gray);
+    --maxWidth: 1000px;
+  }
+`;
